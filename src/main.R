6f21e7514539d1b0c4ddbdfef63307d78a9d32dd
kubasikora <283418@pw.edu.pl>
2020-12-13 13:14:47 +0100

    finished kmedoids

--- src/main.R
+++ src/main.R
@@ -69,6 +69,31 @@ playersPositions <- subset(playersEncoded, select=c("Position"))
 playersData <- subset(playersEncoded, select=-c(Position))
 playersData <- scale(playersData)
 
+positionsAsText <- data.frame(lapply(playersPositions, as.character), stringsAsFactors=FALSE)
+
+mapByOutfieldPosition <- function(x) {
+    if(x == "GK"){
+        return(1) 
+    } else {
+        return(2)
+    }
+}
+
+mapByGeneralPosition <- function(x) {
+    if(x %in% c("GK")){
+        return(1)
+    } else if(x %in% c("LB", "LCB", "CB", "RCB", "RB", "LWB", "RWB")){
+        return(2)
+    } else if(x %in% c("LM","LCM", "LDM", "CDM", "RDM", "LCM", "CM", "RCM", "RM", "LAM", "CAM", "RAM")){
+        return(3)
+    } else {
+        return(4)
+    }
+}
+
+outfieldTrueClustering <- apply(positionsAsText, 1, mapByOutfieldPosition)
+generalTrueClustering <- apply(positionsAsText, 1, mapByGeneralPosition)
+specificTrueClustering <- as.numeric(factor(playersNumeric$Position))
 
 #### select final set of attributes
 playersAttributesFinal <-as.data.frame(subset(playersData, select=c("Age", "Preferred.Foot", "Weak.Foot", 
@@ -125,7 +150,7 @@ summary(clusteringInput)
 
 ### pam version -> k-medioids with euclidean distance
 minK = 2
-maxK = 30
+maxK = 40
 
 print("Euclidean")
 euclideanDistanceMatrix <- dist(clusteringInput, method="Euclidean")
@@ -134,6 +159,9 @@ result.euclidean.pams <- list()
 result.euclidean.centroids <- list()
 result.euclidean.clustering <- list()
 result.euclidean.metrics <- list()
+result.euclidean.compare.outfield <- list()
+result.euclidean.compare.general <- list()
+result.euclidean.compare.specific <- list()
 
 for(i in minK:maxK){
     # find initial clusters for sampled data.frame
@@ -151,8 +179,13 @@ for(i in minK:maxK){
     
     # find metric values 
     result.euclidean.metrics[[i-minK+1]] <- cluster.stats(euclideanDistanceMatrixFull, result.euclidean.clustering[[i-minK+1]])
+    result.euclidean.compare.outfield[[i-minK+1]] <- cluster.stats(euclideanDistanceMatrixFull, result.euclidean.clustering[[i-minK+1]], alt.clustering=outfieldTrueClustering, compareonly=TRUE)
+    result.euclidean.compare.general[[i-minK+1]] <- cluster.stats(euclideanDistanceMatrixFull, result.euclidean.clustering[[i-minK+1]], alt.clustering=generalTrueClustering, compareonly=TRUE)
+    result.euclidean.compare.specific[[i-minK+1]] <- cluster.stats(euclideanDistanceMatrixFull, result.euclidean.clustering[[i-minK+1]], alt.clustering=specificTrueClustering, compareonly=TRUE)
     print(paste("Evaluated metrics for", i, "clusters"))
 }
+rm(euclideanDistanceMatrix)
+rm(euclideanDistanceMatrixFull)
 
 ### pam version -> k-medioids with minkowski distance with p = 3
 
@@ -163,6 +196,9 @@ result.minkowski.pams <- list()
 result.minkowski.centroids <- list()
 result.minkowski.clustering <- list()
 result.minkowski.metrics <- list()
+result.minkowski.compare.outfield <- list()
+result.minkowski.compare.general <- list()
+result.minkowski.compare.specific <- list()
 
 for(i in minK:maxK){
     # find initial clusters for sampled data.frame
@@ -180,8 +216,13 @@ for(i in minK:maxK){
     
     # find metric values 
     result.minkowski.metrics[[i-minK+1]] <- cluster.stats(minkowskiDistanceMatrixFull, result.minkowski.clustering[[i-minK+1]])
+    result.minkowski.compare.outfield[[i-minK+1]] <- cluster.stats(minkowskiDistanceMatrixFull, result.minkowski.clustering[[i-minK+1]], alt.clustering=outfieldTrueClustering, compareonly=TRUE)
+    result.minkowski.compare.general[[i-minK+1]] <- cluster.stats(minkowskiDistanceMatrixFull, result.minkowski.clustering[[i-minK+1]], alt.clustering=generalTrueClustering, compareonly=TRUE)
+    result.minkowski.compare.specific[[i-minK+1]] <- cluster.stats(minkowskiDistanceMatrixFull, result.minkowski.clustering[[i-minK+1]], alt.clustering=specificTrueClustering, compareonly=TRUE)
     print(paste("Evaluated metrics for", i, "clusters"))
 }
+rm(minkowskiDistanceMatrix)
+rm(minkowskiDistanceMatrixFull)
 
 ### 
 
@@ -192,6 +233,9 @@ result.manhattan.pams <- list()
 result.manhattan.centroids <- list()
 result.manhattan.clustering <- list()
 result.manhattan.metrics <- list()
+result.manhattan.compare.outfield <- list()
+result.manhattan.compare.general <- list()
+result.manhattan.compare.specific <- list()
 
 for(i in minK:maxK){
     # find initial clusters for sampled data.frame
@@ -209,8 +253,13 @@ for(i in minK:maxK){
     
     # find metric values 
     result.manhattan.metrics[[i-minK+1]] <- cluster.stats(manhattanDistanceMatrixFull, result.manhattan.clustering[[i-minK+1]])
+    result.manhattan.compare.outfield[[i-minK+1]] <- cluster.stats(manhattanDistanceMatrixFull, result.manhattan.clustering[[i-minK+1]], alt.clustering=outfieldTrueClustering, compareonly=TRUE)
+    result.manhattan.compare.general[[i-minK+1]] <- cluster.stats(manhattanDistanceMatrixFull, result.manhattan.clustering[[i-minK+1]], alt.clustering=generalTrueClustering, compareonly=TRUE)
+    result.manhattan.compare.specific[[i-minK+1]] <- cluster.stats(manhattanDistanceMatrixFull, result.manhattan.clustering[[i-minK+1]], alt.clustering=specificTrueClustering, compareonly=TRUE)
     print(paste("Evaluated metrics for", i, "clusters"))
 }
+rm(manhattanDistanceMatrix)
+rm(manhattanDistanceMatrixFull)
 
 ####
 
@@ -221,6 +270,9 @@ result.correlation.pams <- list()
 result.correlation.centroids <- list()
 result.correlation.clustering <- list()
 result.correlation.metrics <- list()
+result.correlation.compare.outfield <- list()
+result.correlation.compare.general <- list()
+result.correlation.compare.specific <- list()
 
 for(i in minK:maxK){
     # find initial clusters for sampled data.frame
@@ -232,13 +284,52 @@ for(i in minK:maxK){
     
     # find clusters for all examples
     medoids <- playersAttributesFinal[result$medoids,]
-    distances <- dist(playersAttributesFinal, medoids, method="Euclidean")
+    distances <- dist(playersAttributesFinal, medoids, method="correlation")
     result.correlation.clustering[[i-minK+1]] <- apply(distances, 1, which.min)
     print(paste("Found clusters for all examples"))
     
     # find metric values 
-    result.correlation.metrics[[i-minK+1]] <- cluster.stats(euclideanDistanceMatrixFull, result.correlation.clustering[[i-minK+1]])
+    result.correlation.metrics[[i-minK+1]] <- cluster.stats(correlationDistanceMatrixFull, result.correlation.clustering[[i-minK+1]])
+    result.correlation.compare.outfield[[i-minK+1]] <- cluster.stats(correlationDistanceMatrixFull, result.correlation.clustering[[i-minK+1]], alt.clustering=outfieldTrueClustering, compareonly=TRUE)
+    result.correlation.compare.general[[i-minK+1]] <- cluster.stats(correlationDistanceMatrixFull, result.correlation.clustering[[i-minK+1]], alt.clustering=generalTrueClustering, compareonly=TRUE)
+    result.correlation.compare.specific[[i-minK+1]] <- cluster.stats(correlationDistanceMatrixFull, result.correlation.clustering[[i-minK+1]], alt.clustering=specificTrueClustering, compareonly=TRUE)
     print(paste("Evaluated metrics for", i, "clusters"))
 }
+rm(correlationDistanceMatrix)
+rm(correlationDistanceMatrixFull)
 
-## print(result.euclidean.metrics[[1]]$dunn)
+save(result.correlation.centroids, 
+     result.correlation.clustering,
+     result.correlation.pams, 
+     result.correlation.metrics,
+     result.correlation.compare.outfield,
+     result.correlation.compare.general,
+     result.correlation.compare.specific,
+     
+     result.manhattan.pams, 
+     result.manhattan.centroids, 
+     result.manhattan.clustering, 
+     result.manhattan.metrics,
+     result.manhattan.compare.outfield,
+     result.manhattan.compare.general,
+     result.manhattan.compare.specific,
+     
+     result.minkowski.pams, 
+     result.minkowski.clustering, 
+     result.minkowski.centroids,
+     result.minkowski.metrics,
+     result.minkowski.compare.outfield,
+     result.minkowski.compare.general,
+     result.minkowski.compare.specific,
+     
+     result.euclidean.pams, 
+     result.euclidean.clustering, 
+     result.euclidean.centroids, 
+     result.euclidean.metrics,
+     result.euclidean.compare.outfield,
+     result.euclidean.compare.general,
+     result.euclidean.compare.specific,
+     
+     clusteringInput, 
+     file="./kmedoids-results.RData"
+)
